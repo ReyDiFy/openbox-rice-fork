6ad1b61c46af140ff800cd613f58454d1dd0023d
ReyDiFy <148668287+ReyDiFy@users.noreply.github.com>
2026-06-16 13:37:28 +0800

    Update rice.sh

--- rice.sh
+++ rice.sh
@@ -11,8 +11,7 @@ git fetch
 git reset origin/main
 git checkout -t origin/main
 git submodule update --init --recursive
-# install yay
-makepkg -si && cd -
+
 # install packages
 THEME="lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings arc-gtk-theme"
 FONTS="ttf-fira-code ttf-firacode-nerd noto-fonts noto-fonts-cjk noto-fonts-emoji"
